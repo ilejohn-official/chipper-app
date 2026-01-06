96e36c448431df0f99c70fc46d720cae65fa7a2d
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2026-01-06 11:47:33 +0100

    Create posts Pinia store with create action

---
+++ stores/posts.js
@@ -0,0 +1,32 @@
+export const usePosts = defineStore('posts', () => {
+  const { $api } = useNuxtApp()
+
+  const items = ref([])
+
+  const loading = ref(false)
+
+  const error = ref(null)
+
+  async function createPost ({ title, body }) {
+    loading.value = true
+    error.value = null
+
+    try {
+      const response = await $api.post('/posts', { title, body })
+      items.value.unshift(response.data)
+      return response
+    } catch (err) {
+      error.value = err.message || 'Failed to create post'
+      throw err
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    items,
+    loading,
+    error,
+    createPost
+  }
+})
